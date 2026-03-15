5cfa38a00b437622aecb7bfcb1dce7b4db8dadcc
catwithasword <panida.r.work@gmail.com>
2026-03-15 23:48:05 +0700

    Initial commit

---
+++ KartangoWidget/KartangoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  KartangoWidgetBundle.swift
+//  KartangoWidget
+//
+//  Created by Panida Rumriankit on 15/3/2569 BE.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct KartangoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        KartangoWidget()
+    }
+}
